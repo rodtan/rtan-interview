a91d1672a5e2d31165f4b072d1d827d3dc542e4b
Rotherick Tan <rodtan@yahoo.com>
2024-07-03 11:21:57 -0500

    RT-10: User Integration Test

--- src/intTest/java/com/rodtan/interview/UserControllerIT.java
+++ src/intTest/java/com/rodtan/interview/UserControllerIT.java
@@ -11,84 +11,81 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-        classes = Application.class)
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerIT {
-    private MockMvc mvc;
-
-    @Mock
-    private UserService userService;
-
-    @InjectMocks
-    private UserController userController;
+    @Autowired private TestRestTemplate restTemplate;
 
     private JacksonTester<User> UserJacksonTester;
 
     private JacksonTester<List<User>> UserListJacksonTester;
 
-    @BeforeEach
-    public void setUp() {
-        JacksonTester.initFields(this, new ObjectMapper());
-        mvc = MockMvcBuilders.standaloneSetup(userController)
-                .build();
-    }
-
-    public User createUser() {
+    private User createUser() {
         User user = new User(1, "Rod", "rodtest@example.com");
         user.setId(1);
         return user;
     }
-    @Test
-    public void canGetAllUsers() throws Exception {
-        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders.get("/users")).andReturn().getResponse();
-        Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        Assertions.assertThat(response.getContentAsString()).isEqualTo(
-                UserListJacksonTester.write(getAllTestUser()).getJson()
-        );
-    }
-
-    @Test
-    public void canGetUserById() throws Exception {
-        MockHttpServletResponse response = mvc.perform(get("/users/1")).andReturn().getResponse();
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.getContentAsString()).isEqualTo(
-                UserJacksonTester.write(createUser()).getJson()
-        );
-    }
 
     @Test
     public void returnsNotFoundIfUserNotFound() throws Exception {
-        MockHttpServletResponse response = mvc
-                .perform(MockMvcRequestBuilders.get("/users/id/10"))
-                .andReturn()
-                .getResponse();
-        Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        User expected = new User(null, null, null);
+        ResponseEntity<User> responseEntity = restTemplate.getForEntity("/users/1", User.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(responseEntity.getBody()).isEqualTo(expected);
     }
 
-    private List<User> getAllTestUser() {
-        List<User> users = new ArrayList<>();
-        users.add(createUser());
-        return users;
+    @Test
+    public void happyFlow() throws Exception {
+        User user = createUser();
+        ResponseEntity<User> responseEntity = restTemplate.postForEntity("/users", user, User.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(user);
+
+        ResponseEntity<User> responseEntity2 = restTemplate.getForEntity("/users/1", User.class);
+        assertThat(responseEntity2.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity2.getBody()).isEqualTo(user);
+
+        ResponseEntity<User[]> responseEntity3 = restTemplate.getForEntity("/users", User[].class);
+        assertThat(responseEntity3.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity3.getBody()).isNotEmpty();
+        assertThat(responseEntity3.getBody()).contains(user);
+
+        user.setName("RodTan");
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<User> requestEntity = new HttpEntity<>(user, headers);
+        Map<String, String> param = new HashMap<>();
+        ResponseEntity<User> responseEntity4 = restTemplate.exchange("/users/1", HttpMethod.PUT, requestEntity, User.class, param);
+        assertThat(responseEntity4.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity4.getBody()).isEqualTo(user);
+
+        ResponseEntity<User> responseEntity5 = restTemplate.exchange("/users/1", HttpMethod.DELETE, requestEntity, User.class);
+        assertThat(responseEntity5.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
+
 
 }
